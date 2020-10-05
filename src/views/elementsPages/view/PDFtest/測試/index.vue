15f5a55f34cfd12c43bff4452cef6bfb1d428a21
Renwen_Zhang <Renwen_Zhang@systemweb.com.tw>
2020-10-05 16:40:39 +0800

    init

--- src/views/elementsPages/view/PDFtest/測試/index.vue
+++ src/views/elementsPages/view/PDFtest/測試/index.vue
@@ -2,24 +2,17 @@
     <div>PDF 測試</div>
 </template>
 <script>
-
-
+import * as d3 from 'd3'
+import * as html2canvas from 'html2canvas'
+import * as jsPDF from 'jspdf'
 export default {
     data() {
-        return {
-
-        }
+        return {}
     },
-    methods: {
-
-    },
+    methods: {},
     mounted: function() {
-
         //綁定 DOM 之後
     },
-
-
 }
 </script>
-<style>
-</style>
+<style></style>
